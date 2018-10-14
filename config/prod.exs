dbbb9cfbb786c13fe8189da26279278a77fa0985
Dima <dima.pyatkov@gmail.com>
2018-10-14 11:19:49 +0300

    Refactored ENCRYPTION_KEYS retrieval.

--- config/prod.exs
+++ config/prod.exs
@@ -1,5 +1,9 @@
 use Mix.Config
 
+defp get_encryption_keys() do
+  System.get_env("ENCRYPTION_KEYS") || raise "ENCRYPTION_KEYS var is not defined"
+end
+
 # For production, we often load configuration from external
 # sources, such as your system environment. For this reason,
 # you won't find the :http configuration below, but set inside
@@ -61,7 +65,7 @@ config :logger, level: :info
 
 # Configure Encryption.AES
 config :file_surrender, Encryption.AES,
-  keys: System.get_env("ENCRYPTION_KEYS") |> String.split(",", trim: true)
+  keys: get_encryption_keys() |> String.split(",", trim: true)
 
 config :file_surrender, FileSurrenderWeb.Endpoint,
   load_from_system_env: true,
