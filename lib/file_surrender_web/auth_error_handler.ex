9745a511095f51cb9c86231f8ecb6aabb50450f1
Dima <dima.pyatkov@gmail.com>
2018-10-17 13:45:18 +0300

    Added user friendly error message when AuthErrorHandler takes control.

--- lib/file_surrender_web/auth_error_handler.ex
+++ lib/file_surrender_web/auth_error_handler.ex
@@ -9,7 +9,15 @@ defmodule FileSurrender.AuthErrorHandler do
     Logger.debug "Authentication error of type #{type}, reason: #{reason}"
     conn
     # |> configure_session(drop: true)
-    |> put_flash(:error, "Authentication error of type #{type}, reason: #{reason}")
+    |> put_flash(:error, error_message(type, reason))
     |> redirect(to: "/")
+  end
+
+  defp error_message(:unauthenticated, :unauthenticated) do
+    "Please authorize using either Google, Facebook or VK"
+  end
+
+  defp error_message(type, reason) do
+    "Authentication error of type #{type}, reason: #{reason}"
   end
 end
