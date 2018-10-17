49e38eef4c0ba029b5ce066bfc1ed75bcd829899
Dima <dima.pyatkov@gmail.com>
2018-10-17 13:40:02 +0300

    Added logging in AuthErrorHandler and commented session drop in configure_session.

--- lib/file_surrender_web/auth_error_handler.ex
+++ lib/file_surrender_web/auth_error_handler.ex
@@ -3,10 +3,12 @@ defmodule FileSurrender.AuthErrorHandler do
   import Plug.Conn
   import Phoenix.Controller
 
+  require Logger
+
   def auth_error(conn, {type, reason}, _opts) do
-    # raise "Authentication error of type #{type}, reason: #{reason}"
+    Logger.debug "Authentication error of type #{type}, reason: #{reason}"
     conn
-    |> configure_session(drop: true)
+    # |> configure_session(drop: true)
     |> put_flash(:error, "Authentication error of type #{type}, reason: #{reason}")
     |> redirect(to: "/")
   end
