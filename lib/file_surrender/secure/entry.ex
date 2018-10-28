dcaf90f9e753734e6235511541e7f1ba0b330761
Dima <dima.pyatkov@gmail.com>
2018-10-28 18:05:59 +0300

    Added key hash decryption missing step.

--- lib/file_surrender/secure/entry.ex
+++ lib/file_surrender/secure/entry.ex
@@ -36,7 +36,7 @@ defmodule FileSurrender.Secure.Entry do
         import Encryption.Utils
         Logger.debug "Detected a valid changeset"
         Logger.debug "Going to encrypt the raw value of secret: #{inspect secret}"
-        cs |> put_change(:secret, "$V2$_" <> encrypt(key |> Base.encode64, secret))
+        cs |> put_change(:secret, "$V2$_" <> encrypt(user.id |> decrypt_key_hash(key), secret))
       _ -> cs
     end
   end
