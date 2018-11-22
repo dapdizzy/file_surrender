2cad8df68d2d705e6127e02746c9637053d0dcef
Dima <dima.pyatkov@gmail.com>
2018-11-22 15:05:32 +0300

    Swallow nil value for user in main page index.

--- lib/file_surrender_web/controllers/page_controller.ex
+++ lib/file_surrender_web/controllers/page_controller.ex
@@ -7,7 +7,7 @@ defmodule FileSurrenderWeb.PageController do
   def index(conn, _params) do
     user = Guardian.Plug.current_resource(conn)
     action_required = !!(user && Secure.has_entries_by_uid(user.id))
-    render conn, "index.html", current_user: user, action_required: action_required, secret: user.secret
+    render conn, "index.html", current_user: user, action_required: action_required, secret: user && user.secret
     # , code: get_session(conn, :code)
   end
 
